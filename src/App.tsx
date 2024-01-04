9b9a4bbde44ef2222e55cd746fb6c10cdc0763b8
subject026 <subject026@protonmail.com>
2024-01-04 10:52:21 +0000

    add controls

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen bg-orange-400 flex items-center justify-center flex-col gap-12">
       <h1 className="font-bold text-4xl text-purple-800">Heyy :)</h1>
-      <audio src="/forest-rain.mp3" />
+      <audio controls src="/forest-rain.mp3" />
     </div>
   );
 }
