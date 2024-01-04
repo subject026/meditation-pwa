0113f7bf4f1b39a4a958d7e74a842b3d46f442d8
subject026 <subject026@protonmail.com>
2024-01-04 11:04:18 +0000

    try yt vid

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ function App() {
     <div className="min-h-screen bg-orange-400 flex items-center justify-center flex-col gap-12">
       <h1 className="font-bold text-4xl text-purple-800">Heyy :)</h1>
       <audio controls src="/forest-rain.mp3" />
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/uqSz1ddkZvY?si=6RM84r-c1ajXKb6k"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
     </div>
   );
 }
