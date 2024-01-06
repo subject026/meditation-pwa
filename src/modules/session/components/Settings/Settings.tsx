cabff1fde12d8acaffbad20ce1cd948843b610eb
subject026 <subject026@protonmail.com>
2024-01-06 20:11:56 +0000

    tweak styles

--- src/modules/session/components/Settings/Settings.tsx
+++ src/modules/session/components/Settings/Settings.tsx
@@ -13,25 +13,27 @@ export function Settings() {
       defaultValue={sessionLength}
       minValue={0}
       onChange={setSessionLength}
-      className="flex gap-4"
+      className="flex flex-col gap-4"
     >
       <Label>Duration</Label>
       <Group className="flex gap-2">
-        <Input className="text-3xl font-bold bg-transparent" />
-        <div className="grid grid-cols-2 gap-2">
+        <Input className="max-w-52 text-3xl font-bold bg-transparent" />
+        <div className="flex flex-col gap-2">
           <Button
             className={
-              "bg-red-500 text-neutral-300 font-bold text-xl px-4 py-2"
+              "px-4 py-2 rounded bg-red-500 text-neutral-300 font-bold text-lg"
+            }
+            slot="increment"
+          >
+            +
+          </Button>
+          <Button
+            className={
+              "px-4 py-2 rounded bg-red-500 text-neutral-300 font-bold text-xl"
             }
             slot="decrement"
           >
             -
-          </Button>
-          <Button
-            className={"bg-red-500 text-neutral-300 font-bold text-lg p-4"}
-            slot="increment"
-          >
-            +
           </Button>
         </div>
       </Group>
--- src/modules/session/core/componnents/Layout/Layout.tsx
+++ src/modules/session/core/componnents/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-orange-400 flex items-center justify-center flex-col gap-12">
+    <div className="min-h-screen bg-neutral-200 flex items-center justify-center flex-col gap-12">
       {children}
     </div>
   );
