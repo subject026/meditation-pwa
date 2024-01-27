c787be91a826ddffea81b137ef1baa17d2483fd9
subject026 <subject026@protonmail.com>
2024-01-27 08:19:21 +0000

    nav refactoring

--- src/modules/core/components/Layout/Layout.tsx
+++ src/modules/core/components/Layout/Layout.tsx
@@ -1,36 +1,11 @@
-import { Link } from "@tanstack/react-router";
 import { ReactNode } from "react";
+import { Header } from "../Header/Header";
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen bg-neutral-200 flex flex-col gap-12">
+      <Header />
       <main className="grow">{children}</main>
-      <nav className="fixed bottom-0 flex gap-4  py-4">
-        <Link
-          className="px-4 py-2 rounded bg-neutral-900 font-bold text-neutral-300"
-          to="/"
-        >
-          home
-        </Link>
-        <Link
-          className="px-4 py-2 rounded bg-neutral-900 font-bold text-neutral-300"
-          to="/focus"
-        >
-          focus
-        </Link>
-        <Link
-          className="px-4 py-2 rounded bg-neutral-900 font-bold text-neutral-300"
-          to="/session"
-        >
-          session
-        </Link>
-        <Link
-          className="px-4 py-2 rounded bg-neutral-900 font-bold text-neutral-300"
-          to="/pwa-testing"
-        >
-          pwa testing
-        </Link>
-      </nav>
     </div>
   );
 }
